1c4cbb7f9d5a531e4c4e57ce0947c82a5b8464dc
HannahSky <111460985+HannahSky@users.noreply.github.com>
2023-09-28 14:06:05 -0700

    Finished Activity

--- scripts.js
+++ scripts.js
@@ -29,3 +29,25 @@ for(let span of ltBlueSpans){
 for(let span of medBlueSpans){
     span.style.color = "#3C5E73";
 }
+
+//selecting a group with CSS selectors 
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+let svgs = document.querySelectorAll("#svgs svg");
+console.log(svgs);
+
+//iterate through the SVG collection and change the color of each
+for(let i = 0; i < svgs.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
+//individual elements with CSS selector 
+document.querySelector(".bold").style.color = "#7C9EA6";
+
+// changing DOM node content
+document.querySelector(".content_list li:first-of-type").textContent = "this is new list item text";
+
+//this would select all of the li's in the .content_list
+document.querySelectorAll(".content_list li")[2].innerHTML += "<strong> Hannah Kaminsky</strong>"; // the brackets would say pick specifically the number in the idex (0 , 1, 2 ,3)
+
+// removing attributes 
+document.querySelector("#remove a").removeAttribute("hidden");
